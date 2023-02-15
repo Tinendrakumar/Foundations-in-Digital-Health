7d6abf17f8ca3140f4c6aa27e62af59707ad2dc2
Tinendrakumar <58140506+Tinendrakumar@users.noreply.github.com>
2023-02-14 22:07:34 -0500

    Update Circles-in-2022ab UMLS.py

--- Circles-in-2022ab UMLS.py
+++ Circles-in-2022ab UMLS.py
@@ -7,14 +7,14 @@ cnx = mysql.connector.connect(host='172.16.34.1',
                               user='umls',
                               password='umls', database='umls2022')
 
-# Create a cursor to execute queries
+# Creating a cursor to execute queries
 cursor = cnx.cursor()
 
-# Get the CUI relationships from the UMLS database
+# Getting the CUI relationships from the UMLS database
 query = 'SELECT CUI1, CUI2, RELA FROM MRREL'
 cursor.execute(query)
 
-# Build the graph of CUIs and their relationships
+# Building the graph of CUIs and their relationships
 G = nx.DiGraph()
 for cui1, cui2, rela in cursor:
     G.add_edge(cui1, cui2)
@@ -39,7 +39,7 @@ for node in G:
     visited = set()
     path = []
     if find_cycles(node, G, visited, path):
-        path.append(path[0]) # add the first term to the end of the path
+        path.append(path[0])
         if len(path)>3 and len(path)<40:
             print(path)
 
